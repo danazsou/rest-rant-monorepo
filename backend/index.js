0df80e4ab8ca2ee9087658b8ba8e1a227319dd2c
danazou <dana.sou@gmail.com>
2023-02-24 09:54:38 -0800

    authentication files and beginning cookies

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express();
+const cookieSessions=(require)('cookie-session')
 
 // Express Settings
 app.use(cors())
